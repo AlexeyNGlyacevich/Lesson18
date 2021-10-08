1f991309874c057ea87b60f75e0e340ada1c6432
zalikry <zalikry@tut.by>
2021-10-09 00:42:56 +0300

    изменение 1

--- src/Garage.java
+++ src/Garage.java
@@ -1,5 +1,6 @@
 import com.sun.xml.internal.ws.api.message.Message;
 
+
 import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
@@ -9,31 +10,38 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
+
+
 public class Garage {
 
-    public ArrayList<Car> carArrayList = new ArrayList<>();
+    private ArrayList<Car> carArrayList = new ArrayList<>();
+
+    public ArrayList<Car> getCarArrayList() {
+        return carArrayList;
+    }
+
+    public void setCarArrayList(ArrayList<Car> carArrayList) {
+        this.carArrayList = carArrayList;
+    }
 
     public Garage() {
-        carArrayList.add(new Bus(4, 10, "green", "Mersedes", Car.fuelType.Diesel));
-        carArrayList.add (new Bus(5, 22, "read","volvo", Car.fuelType.Diesel));
-        carArrayList.add (new PassengerCar("leather", "yellow", "Ferrari",Car.fuelType.Gasoline));
-        carArrayList.add(new PassengerCar("velur","grey","Audi",Car.fuelType.Gasoline));
+
     }
 
     // Добавление машины в Коллекцию
-    public void AddNewCarToArrayList(Car car){
+    public void addNewCar(Car car){
 
         carArrayList.add(car);
     }
 
     // Удаление машины из Коллекции по имени.
-    public void DeleteCar(String name){
+    public void deleteCar(String name){
 
-        carArrayList.removeIf(car -> car._name.equals(name));
+        carArrayList.removeIf(car -> car.getName().equals(name));
     }
 
-    // Сортировка по Имени машины.
-    public void GarageSortForName(){
+    // Выыод на экран отсотрированной по имени коллекции машин.
+    public void printCarsSortedByName(){
        carArrayList.stream().sorted(new Comparator<Car>() {
            @Override
            public int compare(Car a, Car b) {
@@ -41,18 +49,18 @@ public class Garage {
            }
        }).forEach(car -> System.out.println(car.getClass()+" - "+ car.getName()));
     }
-
-    public void GetDieselCars(){
-        carArrayList.stream().filter(x -> x._type.equals(Car.fuelType.Diesel))
-                    .forEach(s -> System.out.println(s.toString()));
+    // Вывод на экран машин из коллекции по заданному типу топлива
+    public void printCarsSortedByFuelType(Car.FuelType type){
+        carArrayList.stream().filter(x -> x.getType().equals(type))
+                             .forEach(s -> System.out.println(s.toString()));
     }
     // Найти количество машин заданного цвета
-    public int GetNumberOfCars(String color){
+    public int getNumberOfCars(String color){
 
-        return (int) carArrayList.stream().filter(x -> x._color.equals(color)).count();
+        return (int) carArrayList.stream().filter(x -> x.getColor().equals(color)).count();
     }
    // Сохранить коллекцию машин в текстовый файл.
-    public void Savetofile(String fileName, ArrayList<Car> list){
+    public void saveTotxtFile(String fileName, ArrayList<Car> list){
         StringBuilder builder = new StringBuilder();
         for (Car car: list) {
             builder.append(car.toString());
@@ -64,22 +72,43 @@ public class Garage {
         }
     }
 
+    // Cохранить коллекцию машин в JSON/XML файл.
+/*    public void  saveToJSonFile(Garage garage){
+        XStream xstream = new XStream();
+        xstream.setMode(XStream.NO_REFERENCES);
+        xstream.alias("garage", Garage.class);
+        System.out.println(xstream.toXML(garage));
+        try{
+            FileOutputStream fs = new FileOutputStream("C:/Users/IdeaProjects/Lesson17/employeedata.txt");
+         xstream.toXML(garage, fs)
+           } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+   }*/
 
     public static void main(String[] args){
 
         Garage garage = new Garage();
-        garage.GetDieselCars();
-        System.out.println("-----------------");
-        garage.AddNewCarToArrayList(new Bus(6, 25,"red","Nissan", Car.fuelType.Diesel ));
-        garage.GetDieselCars();
-        System.out.println("-----------------");
-        System.out.println(garage.GetNumberOfCars("red"));
-        System.out.println("-----------------");
-        garage.GarageSortForName();
-        System.out.println("-----------------");
-        garage.DeleteCar("volvo");
-        garage.GarageSortForName();
 
-        garage.Savetofile("Garage.txt", garage.carArrayList );
+        garage.carArrayList.add( new Bus("green", "Mersedes", Car.FuelType.Diesel, 4, 10));
+        garage.carArrayList.add( new Bus("read", "volvo", Car.FuelType.Diesel,5, 22));
+        garage.carArrayList.add (new PassengerCar( "yellow", "Ferrari", Car.FuelType.Gasoline, "leather"));
+        garage. carArrayList.add(new PassengerCar("grey","Audi",Car.FuelType.Gasoline,"velur"));
+
+        garage.printCarsSortedByFuelType(Car.FuelType.Diesel);
+        System.out.println("-----------------");
+        garage.addNewCar(new Bus("red", "Nissan",Car.FuelType.Diesel,6, 25));
+        garage.printCarsSortedByFuelType(Car.FuelType.Diesel);
+        System.out.println("-----------------");
+        System.out.println(garage.getNumberOfCars("red"));
+        System.out.println("-----------------");
+        garage.printCarsSortedByName();
+        System.out.println("-----------------");
+        garage.deleteCar("volvo");
+        garage.printCarsSortedByName();
+
+
+        garage.saveTotxtFile("Garage.txt", garage.carArrayList );
+     //   garage.saveToJSonFile(garage);
     }
 }
